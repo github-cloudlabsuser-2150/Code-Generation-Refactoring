9a63996e8350fbdb1a19d26c1b4b6d62985ca34f
Jose Luis Melo Rojas <github_cloudlabsuser_2150@cloudlabsaiuser.com>
2025-03-27 14:26:18 +0000

    Add basic calculator with addition, subtraction, multiplication, and division functions

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Create a basic calculator with basic operations like: addition, subtraction, multiplication, and division
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+    
